92e98316535faa8f35858c4531b56f447c7649c4
Simon Zhang <zhanghan.simon@gmail.com>
2021-10-03 16:32:50 +0800

    update circleci config to new aws s3 deployment

--- frontend/deploy_mainnet.sh
+++ frontend/deploy_mainnet.sh
@@ -4,8 +4,8 @@
 NODE_ENV=production yarn build
 
 # copy dist to mainnet S3
-aws s3 sync ./build/ s3://voltswap-farm-main/ --delete --acl public-read
+aws s3 sync ./build/ s3://farm.voltswap.finance/ --delete --acl public-read
 
 # invalidate mainnet cloudfront cache
-aws cloudfront create-invalidation --distribution-id E206N8M1OFV7ZA --paths "/*"
+aws cloudfront create-invalidation --distribution-id E2CWEJFAUKH330 --paths "/*"
 
