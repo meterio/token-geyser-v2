73de23ec614fa6b30acb3d40495df46d65267179
philipappiah <pappiahkubi1@gmail.com>
2021-12-01 05:12:01 +0000

    fix subgraph issues

--- frontend/src/config/geyser.ts
+++ frontend/src/config/geyser.ts
@@ -7,17 +7,64 @@ import { GeyserConfig } from '../types'
  *
  */
 
-
-
-
-
 const mockGeyserConfigs: GeyserConfig[] = [
   {
-    name: 'TFUEL-VOLT-VOLT_AIR',
+    name: 'TFUEL-VOLT (Test)',
     address: '0xbd515e41df155112cc883f8981cb763a286261be',
     stakingToken: StakingToken.MOCK,
     rewardToken: RewardToken.MOCK,
+  },
+
+  {
+    name: 'TFUEL-BUSD.bsc',
+    address: '0x743e7034652b7555Ed96c280Ae89f5EC0383696f',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
+  },
+  {
+    name: 'BUSD.bsc-USDC.eth',
+    address: '0x02F7214305f8e4419d6B2Fe10A56E9566e6c388E',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
+  },
+  {
+    name: 'TFUEL-WETH.eth',
+    address: '0x139B2305dA9D000dde725203857d9CA90d58DB3b',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
+  },
+  {
+    name: 'TFUEL-USDC.eth',
+    address: '0x5EFE36Df7235E9BB49a52D147fdaFAe6298520A0',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
+  },
+  {
+    name: 'TFUEL-BNB.bsc',
+    address: '0xA1cB648E50527B954E36a9E9c0E8ca29942b38c2',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
+  },
+  {
+    name: 'TFUEL-VOLT',
+    address: '0xF0F5ef5aB3CBAf7A0cE4cecc1Ab0285123954f2E',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
+  },
+  {
+    name: 'BUSD.bsc-VOLT',
+    address: '0x0E346abF5A93fF41E9368009eE0A0a6d54fB47fa',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
+  },
+  {
+    name: 'TFUEL-MTRG',
+    address: '0x702dA5e934A4f1b1C1e8fD8b3190ddb537D1108D',
+    stakingToken: StakingToken.MOCK,
+    rewardToken: RewardToken.MOCK,
   }
+  
+  
 ]
 
 export const geyserConfigs: GeyserConfig[] = mockGeyserConfigs
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -68,7 +68,7 @@ export enum RewardToken {
 
 // subgraph endpoint
 
-export const GEYSER_SUBGRAPH_ENDPOINT = `http://13.229.153.186:8000/subgraphs/name/theta/geyser-v2`
+export const GEYSER_SUBGRAPH_ENDPOINT = `http://13.214.99.74:8000/subgraphs/name/theta/geyser-v2`
 
 // ufragments deploy block number
 export const UFRG_INIT_BLOCK = 7947823
