0fd757e28537e1c77ad7f758d3ad1f4bc5ac109a
philipappiah <pappiahkubi1@gmail.com>
2021-12-02 21:33:58 +0000

    fix volt price issue

--- frontend/src/utils/voltPrice.ts
+++ frontend/src/utils/voltPrice.ts
@@ -1,14 +1,14 @@
-// import { Contract } from 'ethers'
-// import BigNumber from 'bignumber.js'
-// import { getCurrentPrice } from './price'
+import { Contract } from 'ethers'
+import BigNumber from 'bignumber.js'
+import { getCurrentPrice } from './price'
 import { SignerOrProvider } from '../types'
-// import { abi as IUniswapV2Pair } from '../sdk/IUniswapV2Pair.json'
+import { abi as IUniswapV2Pair } from '../sdk/IUniswapV2Pair.json'
 
 export const estimateVoltPrice = async (signerOrProvider: SignerOrProvider) => {
-  console.log(signerOrProvider)
-  // const mtrgPrice = await getCurrentPrice('MTRG')
-  // const mtrgVoltPair = new Contract('0x1071392e4cdf7c01d433b87be92beb1f8fd663a8', IUniswapV2Pair, signerOrProvider)
-  // const { reserve0, reserve1 } = await mtrgVoltPair.getReserves()
+ 
+  const mtrgPrice = await getCurrentPrice('MTRG')
+  const mtrgVoltPair = new Contract('0xbd346458ad37f2d3101ede54cb411d2636decbc6', IUniswapV2Pair, signerOrProvider)
+  const { reserve0, reserve1 } = await mtrgVoltPair.getReserves()
   // console.log(
   //   'estimate VOLT price, MTRG price: ',
   //   mtrgPrice,
@@ -17,8 +17,8 @@ export const estimateVoltPrice = async (signerOrProvider: SignerOrProvider) => {
   //   ', VOLT amount: ',
   //   new BigNumber(reserve1.toString()).div(1e18).toFixed(4),
   // )
-  // const price = new BigNumber(mtrgPrice).times(reserve0.toString()).div(reserve1.toString()).toNumber()
-  const price = 0.4
-  // console.log('est. VOLT price: ', price)
+  const price = new BigNumber(mtrgPrice).times(reserve0.toString()).div(reserve1.toString()).toNumber()
+ 
+  console.log('est. VOLT price: ', price)
   return price
 }
