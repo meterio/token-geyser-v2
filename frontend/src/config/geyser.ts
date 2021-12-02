3d78b6a6cc51c9469c9daa390cd111e80cdfc2d3
philipappiah <pappiahkubi1@gmail.com>
2021-12-02 20:02:03 +0000

    change volt single pool address

--- frontend/src/config/geyser.ts
+++ frontend/src/config/geyser.ts
@@ -12,7 +12,7 @@ const mockGeyserConfigs: GeyserConfig[] = [
     name:'VOLT',
     stakingToken: StakingToken.VOLT,
     rewardToken: RewardToken.MOCK,
-    address:'0xbd515e41df155112cc883f8981cb763a286261be'
+    address:'0xCd872033f3Ed9227BC78F47fB0E0DFf7dbDBE5B4'
   },
   {
     name: 'TFUEL-VOLT (Test)',
