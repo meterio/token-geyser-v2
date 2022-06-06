d68bcda47c11276790afd38415cb9b87e0493694
philipappiah <pappiahkubi1@gmail.com>
2022-06-06 17:33:05 +0100

    change moonbeam endpoint

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -68,7 +68,7 @@ export enum RewardToken {
 
 // subgraph endpoint
 
-export const GEYSER_SUBGRAPH_ENDPOINT = `https://geyser-graph-on-moonbeam.voltswap.finance/subgraphs/name/moonbeam/token-geyser-v2`
+export const GEYSER_SUBGRAPH_ENDPOINT = `https://graph-moonbeam.voltswap.finance/subgraphs/name/moonbeam/token-geyser-v2`
 
 // ufragments deploy block number
 export const UFRG_INIT_BLOCK = 7947823
